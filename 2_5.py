9dae8ce75ffa4aa435bd64e46a7e5a42e6a71a67
Lev S <firemail.leo@gmail.com>
2021-08-18 15:07:42 +0300

    upd 5 (completed), add 6-9 completed

--- 2_5.py
+++ 2_5.py
@@ -1,16 +1,18 @@
 # 2.5 Вывести на экран коды и символы таблицы ASCII, начиная с символа под номером 32 и заканчивая 127-м включительно.
 # Вывод выполнить в табличной форме: по десять пар "код-символ" в каждой строке.
 
+
 stamp = ""
 stamp_counter = 0
+start = 32
+end = 127
 
-for _ in range(32, 127):
-    if stamp_counter < 10:
-        stamp += f" '{_} – {chr(_)}' "
-        stamp_counter += 1
-        # TODO: пропускается каждая 11-я пара
-    else:
+for code in range(start, end + 1):
+    stamp_counter += 1
+    stamp += f" '{code} – {chr(code)}' "
+    if stamp_counter == 10:
         print(stamp)
         stamp = ""
         stamp_counter = 0
-# TODO: не возвращается остаток символов, которых меньше 10
+    if code == end:
+        print(stamp)
